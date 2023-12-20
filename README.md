83853cdba2b03ad6dadd56527b791a90792d4b67
RevaPawar <132827386+RevaPawar@users.noreply.github.com>
2023-12-20 11:13:46 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# PBL_Sem_4
+This is our PBL Project which we did in our 4th Semester. 
---
+++ myscript.js
@@ -0,0 +1,16 @@
+// setTimeout(()=>{
+// alert("Hello welcome to the site")
+// },3000)
+
+$("#search-icon").click(function() {
+    $(".nav").toggleClass("search");
+    $(".nav").toggleClass("no-search");
+    $(".search-input").toggleClass("search-active");
+  });
+  
+  $('.menu-toggle').click(function(){
+     $(".nav").toggleClass("mobile-nav");
+     $(this).toggleClass("is-active");
+  });
+
+
